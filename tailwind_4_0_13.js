06622ea9a5e62e06a890a18c6e42525cd013f89e
Marcus Schappi <marcus@schappi.com>
2025-03-12 19:58:29 +1100

    remove chrome warnings from Tailwind

--- tailwind_4_0_13.js
+++ tailwind_4_0_13.js
@@ -10971,9 +10971,7 @@ input:where([type='button'], [type='reset'], [type='submit']),
       throw (performance.mark("(error)", { detail: { error: `${r}` } }), r);
     }
   };
-  console.warn(
-    "The browser build of Tailwind CSS should not be used in production. To use Tailwind CSS in production, use the Tailwind CLI, Vite plugin, or PostCSS plugin: https://tailwindcss.com/docs/installation",
-  );
+
   var eo = "text/tailwindcss",
     Xe,
     St = new Set(),
